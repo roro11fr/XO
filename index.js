13025751efc201b69e9d9694532e1d24a7d26db0
roro11fr <94123839+roro11fr@users.noreply.github.com>
2023-05-19 20:01:14 +0300

    create tabel game

---
+++ index.js
@@ -0,0 +1,7 @@
+(function (){
+    let box1 = document.getElementById("box1");
+    const letter = 'X';
+    box1.addEventListener("click", function () {
+        box1.innerHTML = letter;
+    })
+})();
